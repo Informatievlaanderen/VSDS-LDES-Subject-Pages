2ac34d1a5e532d4f1b0e4abbecb4e5f09a205303
jonasbul <jonas.bulcke@cegeka.com>
2024-01-05 22:06:41 +0100

    feat: subject pages (without url safe subjects)

---
+++ repository-initializer/initialize.sh
@@ -0,0 +1,2 @@
+apt-get update && apt-get install curl -y && apt-get clean
+curl -X PUT http://sparql-host:8080/rdf4j-server/repositories/test -H "Content-Type: text/turtle" -d "@/initializer/test.config.ttl"
